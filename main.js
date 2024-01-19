70a46e89e8fe14e4d083a3d36409ed16a654f771
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-19 16:03:09 +0100

    have bug

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.css';
 
 // CODING
 
@@ -6,14 +6,14 @@ import './style.css'
 const colors = ['blue', 'red', 'green', 'black', 'purple'];
 
 const getRandomNumber = (start, end) => {
-  return Math.floor(randomNumber * ((end+1) - start)) + start;
-} 
+  return Math.floor(randomNumber * ((end + 1) - start)) + start;
+};
 
 const randomNumber = Math.random();
 const rand2 = randomNumber * 10;
-const rand3 = Math.floor(randomNumber* 10);
-const rand4 = Math.floor(randomNumber* 10) + 1;
-const rand5 = Math.floor(randomNumber* 3) + 3;
+const rand3 = Math.floor(randomNumber * 10);
+const rand4 = Math.floor(randomNumber * 10) + 1;
+const rand5 = Math.floor(randomNumber * 3) + 3;
 
 // 0 | 3
 // 1 | 4
@@ -22,11 +22,25 @@ const rand5 = Math.floor(randomNumber* 3) + 3;
 const colorIndex = getRandomNumber(0, 4);
 const color = colors[colorIndex];
 
+let coloredBoxesHtml = '';
+for (let i = 0; i < 18; i++) {
+  const colorOfSingleBox = 'yellow';
+  coloredBoxesHtml += `
+<div class="box" style="background-color: ${coloredBoxesHtml}"></div>
+`;
+}
+
+
 
 
 // HTML
 document.querySelector('#app').innerHTML = /*html*/ `
 <h1>While/Do and Math.random()</h1>
+
+<h2>Random colored boxes</h2>
+<div class="boxes">
+${coloredBoxesHtml}
+</div>
 
 <h2>Math.random()</h2>
 <ul>
@@ -35,12 +49,13 @@ document.querySelector('#app').innerHTML = /*html*/ `
   <li>between 0 and 9: <span>${rand3}</span></li>
   <li>between 1 and 10: <span>${rand4}</span></li>
   <li>between 3 and 5: <span>${rand5}</span></li>
-  <li>between 2 and 3: <span>${getRandomNumber(2,3)}</span></li>
-  <li>between 21 and 29: <span>${getRandomNumber(21,29)}</span></li>
+  <li>between 2 and 3: <span>${getRandomNumber(2, 3)}</span></li>
+  <li>between 21 and 29: <span>${getRandomNumber(21, 29)}</span></li>
 </ul>
 
 <h2>Random colored box</h2>
 <p>Color index is ${colorIndex}</p>
 <div class="box" style="background-color: ${color}"></div>
 
-`
+
+`;
